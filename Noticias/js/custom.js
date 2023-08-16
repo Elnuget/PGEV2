9dfffaabcef65e4be08f838a6aebaa3cc62a6eb6
Elnuget <cangulo009@outlook.es>
2023-08-15 22:44:21 -0500

    navbar terminado

---
+++ Noticias/js/custom.js
@@ -0,0 +1,64 @@
+// Custom Script
+// Developed by: Samson.Onna
+// CopyRights : http://webthemez.com
+var customScripts = {
+    profile: function () {
+     	 var portfolio = $('#portfolio');
+		var items = $('.items', portfolio); 
+		var filters = $('.filters li a', portfolio); 
+	
+		items.imagesLoaded(function() {
+			items.isotope({
+				itemSelector: '.item',
+				layoutMode: 'fitRows',
+				transitionDuration: '0.7s'
+			});
+		});
+		
+		filters.click(function(){
+			var el = $(this);
+			filters.removeClass('active');
+			el.addClass('active');
+			var selector = el.attr('data-filter');
+			items.isotope({ filter: selector });
+			return false;
+		});            
+    },
+    fancybox: function () {
+        // fancybox
+        $(".fancybox").fancybox();
+    },
+    
+    bannerHeight: function () {
+        var bHeight = $(".banner-container").height();
+        $('#da-slider').height(bHeight);
+        $(window).resize(function () {
+            var bHeight = $(".banner-container").height();
+            $('#da-slider').height(bHeight);
+        });
+    },
+	waySlide: function(){
+		  	/* Waypoints Animations
+		   ------------------------------------------------------ */		   			  
+		 
+			 						 
+		},
+		fitText: function(){			  
+				setTimeout(function() {			
+				$('h1.responsive-headline').fitText(1.2, { minFontSize: '16px', maxFontSize: '30px' });			
+				}, 100);
+		},
+    init: function () {
+        
+        customScripts.profile();
+        customScripts.fancybox(); 
+        customScripts.owlSlider();
+		customScripts.waySlide();
+		customScripts.fitText();
+        customScripts.bannerHeight();
+    }
+}
+$('document').ready(function () {
+    customScripts.init(); 
+	$('.carousel').carousel();
+});

ce757b40a08bcff5a282034c86cb9260b4b03db1
Elnuget <cangulo009@outlook.es>
2023-08-15 22:04:48 -0500

    funcionamiento nav inicio

--- js/custom.js
+++ js/custom.js
@@ -28,57 +28,7 @@ var customScripts = {
         // fancybox
         $(".fancybox").fancybox();
     },
-    onePageNav: function () {
-
-        $('#mainNav').onePageNav({
-            currentClass: 'active',
-            changeHash: false,
-            scrollSpeed: 950,
-            scrollThreshold: 0.2,
-            filter: '',
-            easing: 'swing',
-            begin: function () {
-                //I get fired when the animation is starting
-            },
-            end: function () {
-                   //I get fired when the animation is ending
-				if(!$('#main-nav ul li:first-child').hasClass('active')){
-					$('.header').addClass('addBg');
-				}else{
-						$('.header').removeClass('addBg');
-				}
-				
-            },
-            scrollChange: function ($currentListItem) {
-                //I get fired when you enter a section and I pass the list item of the section
-				if(!$('#main-nav ul li:first-child').hasClass('active')){
-					$('.header').addClass('addBg');
-				}else{
-						$('.header').removeClass('addBg');
-				}
-			}
-        });
-		
-		$("a[href='#top']").click(function () {
-                $("html, body").animate({ scrollTop: 0 }, "slow");
-                return false;
-            });
-			$("a[href='#basics']").click(function () {
-                $("html, body").animate({ scrollTop: $('#services').offset().top - 75 }, "slow"); 
-                return false;
-            });
-    }, 
-    owlSlider: function () {
-        var owl = $("#owl-demo");
-        owl.owlCarousel();
-        // Custom Navigation Events
-        $(".next").click(function () {
-            owl.trigger('owl.next');
-        })
-        $(".prev").click(function () {
-            owl.trigger('owl.prev');
-        })
-    },
+    
     bannerHeight: function () {
         var bHeight = $(".banner-container").height();
         $('#da-slider').height(bHeight);
@@ -99,7 +49,7 @@ var customScripts = {
 				}, 100);
 		},
     init: function () {
-        customScripts.onePageNav();
+        
         customScripts.profile();
         customScripts.fancybox(); 
         customScripts.owlSlider();
